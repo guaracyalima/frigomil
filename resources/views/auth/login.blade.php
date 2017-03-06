c778de5211c05407aa3ae30c51192f775c4d141f
guaracyalima <guaracyaraujolima@gmail.com>
2017-03-06 14:54:51 -0300

    Novas atualizações

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,100 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Frigomi - Admin :: Login</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <!-- ===== FAVICON =====-->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- ===== CSS =====-->
+    <!-- General-->
+    <link rel="stylesheet" href="css/basic.css">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/theme.css" class="style-theme">
+    
+    <!--[if lt IE 9]>
+    <script src="js/basic/respond.min.js"></script>
+    <script src="js/basic/html5shiv.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="login-bg">
+    
+    <!--[if lt IE 9]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <!--SECTION-->
+    <section class="l-main-container">
+      <!--Main Content-->
+      <div class="login-wrapper">
+        <div class="login-container">
+          <!--Logo-->
+          <h1 class="login-logo"><img src="img/logo.png" alt="Proteus"></h1>
+          <!--Login Form-->
+          <form id="loginForm" role="form" method="POST" action="{{ route('login') }}" class="login-form">
+            {{ csrf_field() }}
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+              <input id="email" type="email" name="email" placeholder="Email" class="form-control">
+              @if ($errors->has('email'))
+              <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+              @endif
+            </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+              <input id="password" type="password" name="password" placeholder="Password" class="form-control">
+              @if ($errors->has('password'))
+              <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+              @endif
             </div>
+
+
+            <button type="submit" class="btn btn-dark btn-block btn-login">Sign In</button>
+            <div class="login-social">
+              <div class="l-span-md-12">
+                <div class="or"><span>- Ou -</span></div>
+              </div>
+              <div class="l-col-sm-6"><a href="#" class="btn btn-facebook btn-block"><i class="fa fa-facebook"></i>Conectar com o Facebook</a></div>
+              <div class="l-col-sm-6"><a href="#" class="btn btn-twitter btn-block"><i class="fa fa-twitter"></i>Conectar com o Twitter</a></div>
+            </div>
+            <div class="login-options"><a href="{{ route('password.request') }}" class="fl">Esqueceu sua senha?</a><a href="page-register.html" class="fr">Cadastre-se</a></div>
+          </form>
         </div>
-    </div>
-</div>
-@endsection
+      </div>
+    </section>
+    <!-- ===== JS =====-->
+    <!-- jQuery-->
+    <script src="js/basic/jquery.min.js"></script>
+    <script src="js/basic/jquery-migrate.min.js"></script>
+    <!-- General-->
+    <script src="js/basic/modernizr.min.js"></script>
+    <script src="js/basic/bootstrap.min.js"></script>
+    <script src="js/shared/jquery.asonWidget.js"></script>
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/general.js"></script>
+    <!-- Semi general-->
+    <script type="text/javascript">
+      var paceSemiGeneral = { restartOnPushState: false };
+      if (typeof paceSpecific != 'undefined'){
+      	var paceOptions = $.extend( {}, paceSemiGeneral, paceSpecific );
+      	paceOptions = paceOptions;
+      }else{
+      	paceOptions = paceSemiGeneral;
+      }
+      
+    </script>
+    <script src="js/plugins/pageprogressbar/pace.min.js"></script>
+    <!-- Specific-->
+    <script src="js/plugins/forms/validation/jquery.validate.min.js"></script>
+    <script src="js/plugins/forms/validation/jquery.validate.additional.min.js"></script>
+    <script src="js/calls/page.login.js"></script>
+  </body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,117 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Frigomil :: Register</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <!-- ===== FAVICON =====-->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- ===== CSS =====-->
+    <!-- General-->
+    <link rel="stylesheet" href="{{ asset('admin/css/basic.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/general.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/css/theme.css') }}" class="style-theme">
+    
+    <!--[if lt IE 9]>
+    <script src="js/basic/respond.min.js"></script>
+    <script src="js/basic/html5shiv.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="login-bg">
+    
+    <!--[if lt IE 9]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <!--SECTION-->
+    <section class="l-main-container">
+      <!--Main Content-->
+      <div class="login-wrapper register-wrapper">
+        <div class="login-container">
+          <!--Logo-->
+          <h1 class="login-logo"><img src="img/logo.png" alt="Proteus"></h1>
+          <!--Login Form-->
+          <form id="registerForm" role="form" action="index.html" class="login-form">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+            {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+              <input id="name" type="text" name="name" placeholder="Name" class="form-control">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
+              @if ($errors->has('name'))
+              <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+              @endif
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+            </div>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+              <input id="email" type="email" name="email" placeholder="Email" class="form-control">
+              @if ($errors->has('email'))
+              <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+              @endif
+            </div>
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+              <input id="password" type="password" name="password" placeholder="Password" class="form-control">
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+              @if ($errors->has('password'))
+              <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+              @endif
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
+            <div class="form-group">
+              <input id="password-confirm" type="password" name="password-confirm" placeholder="Repeat password" class="form-control">
+            </div>
+            <div class="checkbox">
+              <input id="registerTerms" type="checkbox" name="registerTerms" class="checkradios checkradiosDark-1">By signing up you are accepting out
+              <a href="#">Terms and Conditions</a>
+            </div>
+            <button type="submit" class="btn btn-dark btn-block btn-login">Cadastrar</button>
+            <div class="login-social">
+              <div class="l-span-md-12">
+                <div class="or"><span>- Ou -</span></div>
+              </div>
+              <div class="l-span-md-12 register-sign-in"><a href="login.blade.php" class="btn btn-primary btn-block btn-login-register">Login</a></div>
+              <div class="l-col-sm-6"><a href="#" class="btn btn-facebook btn-block"><i class="fa fa-facebook"></i>Entrar com o Facebook</a></div>
+              <div class="l-col-sm-6"><a href="#" class="btn btn-twitter btn-block"><i class="fa fa-twitter"></i>Entrar com o Twitter</a></div>
+            </div>
+            <div class="login-options"></div>
+          </form>
         </div>
-    </div>
-</div>
-@endsection
+      </div>
+    </section>
+    <!-- ===== JS =====-->
+    <!-- jQuery-->
+    <script src="{{ asset('admin/js/basic/jquery.min.js') }}"></script>
+    <script src="js/basic/jquery-migrate.min.js"></script>
+    <!-- General-->
+    <script src="js/basic/modernizr.min.js"></script>
+    <script src="js/basic/bootstrap.min.js"></script>
+    <script src="js/shared/jquery.asonWidget.js"></script>
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/general.js"></script>
+    <!-- Semi general-->
+    <script type="text/javascript">
+      var paceSemiGeneral = { restartOnPushState: false };
+      if (typeof paceSpecific != 'undefined'){
+      	var paceOptions = $.extend( {}, paceSemiGeneral, paceSpecific );
+      	paceOptions = paceOptions;
+      }else{
+      	paceOptions = paceSemiGeneral;
+      }
+      
+    </script>
+    <script src="js/plugins/pageprogressbar/pace.min.js"></script>
+    <!-- Specific-->
+    <script src="js/plugins/forms/elements/jquery.checkradios.min.js"></script>
+    <script src="js/plugins/forms/validation/jquery.validate.min.js"></script>
+    <script src="js/plugins/forms/validation/jquery.validate.additional.min.js"></script>
+    <script src="js/calls/page.register.js"></script>
+  </body>
+</html>
